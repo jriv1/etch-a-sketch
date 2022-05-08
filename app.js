770d34066143514f46fa6311b2b8caf056eacf15
Jorge Villanueva <jriv11011101@outlook.com>
2022-05-08 20:24:56 +0800

    Finished grid

--- app.js
+++ app.js
@@ -2,15 +2,25 @@
 
 const container = document.querySelector(".container"); //container for all grids
 
-for(let i = 0; i < 64 ;i ++){  // loops thru 16 times to make 16 divs // make a function
-    //where user can input number of grids they want 
 
-    const grid = document.createElement("div");
+
+for(let i = 0; i < 256 ;i ++){  // loops thru 16 times to make 16 divs // make a function
+    //where user can input number of grids they want 
     
+     const grid = document.createElement("div");
     document.body.appendChild(grid);
     grid.classList.add("square");
-    grid.innerHTML = "<p>Hello Jorge</p>";
+    grid.innerHTML = "<p></p>";
     container.appendChild(grid);
+    
+    
+    const changeColor = function(){
+        grid.style.backgroundColor = "red";
+    }
+
+  grid.addEventListener("mousemove",changeColor);
+
 
 }
+
 
