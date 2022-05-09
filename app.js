767b2ef864fbb1c4c51b05ea3624cb24febb048d
Jorge Villanueva <jriv11011101@outlook.com>
2022-05-09 20:30:39 +0800

    Finalize style and buttons

--- app.js
+++ app.js
@@ -1,26 +1,72 @@
 
 
-const container = document.querySelector(".container"); //container for all grids
+const container = document.querySelector(".container");
+//container for all grids
+let randomR = Math.trunc(Math.random() * 256);
+let randomG = Math.trunc(Math.random() * 256);
+let randomB = Math.trunc(Math.random() * 256);
 
 
 
-for(let i = 0; i < 256 ;i ++){  // loops thru 16 times to make 16 divs // make a function
-    //where user can input number of grids they want 
+let gridSize = prompt("Number of squares? Max : 100");
+if(gridSize < 100){
+    setUpGrid();
+}
+else{
+    do {
+         gridSize = prompt("Too many squares. Max:100");
+    } while (gridSize > 100);
+    setUpGrid();
+}
+
+
+
+function setUpGrid() {
+    container.style.gridTemplateColumns = `repeat(${gridSize},minmax(0, 1fr))`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, minmax(0, 1fr))`;
     
-     const grid = document.createElement("div");
-    document.body.appendChild(grid);
-    grid.classList.add("square");
-    grid.innerHTML = "<p></p>";
-    container.appendChild(grid);
-    
-    
-    const changeColor = function(){
-        grid.style.backgroundColor = "red";
+    for(let i = 0; i < gridSize * gridSize ;i ++){  // loops thru 16 times to make 16 divs // make a function
+        //where user can input number of grids they want 
+        
+        const grid = document.createElement("div");
+        document.body.appendChild(grid);
+        grid.classList.add("square");
+        grid.innerHTML = "<p></p>";
+        container.appendChild(grid);
+
+
+
+        const changeColor = function(){
+
+            
+            
+                grid.style.background = `rgb(${randomR},${randomG},${randomB})`;       
+           
+
+        }
+
+
+        grid.addEventListener("mouseover",changeColor);
     }
-
-  grid.addEventListener("mousemove",changeColor);
 
 
 }
+
+    
+
+
+
+
+
+
+   
+
+    
+   
+        
+          
+        
+
+
 
 
